2dbca0cb4bb97abe8b7ce816d3903d6b1a9a8f7d
JaWitold <witekkaras00@gmail.com>
2022-03-12 12:19:45 +0100

    wczytywanie grafów do instancji networkx

--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 import sys
 
-from src.read_tsp_instance import read_tsp_instance
+from readTSPInstance import ReadTSPInstance
 
 if len(sys.argv) < 2:
-  exit(1)
+    exit(1)
 
-i = read_tsp_instance(sys.argv[1])
+i = ReadTSPInstance(sys.argv[1])
 i.read()
 
-# print(f"matrix {len(i.matrix)}")
-# print(f"graph {len(i.graph)}")
+print(f"matrix {len(i.matrix)}")
+print(f"graph {len(i.graph)}")
+print(i.graph)
 
-print(f"Failed {sys.argv[1]}" if len(i.matrix) == 0 and len(i.graph) == 0 and 'GEO' in i.header else "Passed")
+print(f"Failed {sys.argv[1]}" if len(i.matrix) == 0 and len(i.graph) == 0 and
+                                 ('GEO' in i.header or 'UPPER_ROW' in i.header) else "Passed")
