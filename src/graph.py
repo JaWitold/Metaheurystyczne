c1a693f32eb1794b27a9b92bb874c1cf850db172
41761707 <wojtczakov@gmail.com>
2022-03-18 00:36:54 +0100

    Added raph generation with solver using given algorithms: k-random, N_N, 2-OPT. Implemented visual representation of results

--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,7 @@
 from matplotlib import pyplot as plt
+import sys
+import random 
+import copy
 
 class Graph:
     supported_formats = ['FULL_MATRIX', 'EUC_2D', 'LOWER_DIAG_ROW']
@@ -9,6 +12,7 @@ class Graph:
     edge_weight_format = ""
     
     header = dict()
+    optimal={"berlin52.tsp": 7542 ,"br17.atsp": 39,"gr120.tsp": 6942 }
     dimension = 0
     matrix = []
     coordinates = dict()
@@ -61,7 +65,7 @@ class Graph:
         numbers = self.read_numbers(file)
         for i in range(0, self.dimension):
             for j in range(0, self.dimension):
-                self.matrix[i][j] = numbers[i * self.dimension + j]
+                self.matrix[i][j] = int(numbers[i * self.dimension + j])
     
     def read_data_from_lower_diag_row(self, file):
         numbers = self.read_numbers(file)
@@ -101,8 +105,121 @@ class Graph:
                 plt.plot(node['x'], node['y'], color="blue", marker="o", markersize=2)
             plt.show()
 
+    def k_random_method(self):
+        k=100
+        print("k: ",k)
+        min_dist=sys.maxsize
+        vertex=[x for x in range(self.dimension)]
+        path=[]
+        for j in range(k):
+            random.shuffle(vertex)
+            distance=0
+            for i in range(len(vertex)):
+                if i+1==len(vertex):
+                    distance=distance+int(self.matrix[vertex[i]][vertex[0]])
+                    break
+                distance=distance+int(self.matrix[vertex[i]][vertex[i+1]])
+            if(distance<min_dist):
+                min_dist=distance
+                path=vertex.copy()
+        print("Droga: ",min_dist)
+        print("Cykl: ",path)
+        if self.edge_weight_format == 'EUC_2D':
+            self.draw_solution(path)
+        self.PRD(min_dist)
+
+    def nearest_neighbor(self):
+        start=random.randint(0,self.dimension-1)
+        print(start)
+        path=[start]
+        min_dist=0
+        matrix_copy=copy.deepcopy(self.matrix)
+        while(len(path)!=self.dimension):
+            distances=matrix_copy[start]
+            distances.sort()
+            counter=0
+            j=0
+            while j < self.dimension:
+                if distances[counter]==self.matrix[start][j]:
+                    if j not in path:
+                        if(distances[counter]==0):
+                            counter=counter+1
+                            j=-1
+                        else:
+                            min_dist=min_dist+int(distances[counter])
+                            path.append(j)
+                            start=j
+                            counter=0
+                            break
+                j=j+1
+                if(j==self.dimension):
+                    j=0 
+                    counter=counter+1
+                if(counter==self.dimension):
+                    print("Ślepy zaułek")
+                    return
+        print("Droga: ",min_dist)
+        print("Cykl: ",path)
+        if self.edge_weight_format == 'EUC_2D':
+            self.draw_solution(path)
+        self.PRD(min_dist)
+
+    def extended_nearest_neighbor(self):
+        pass
+
+    def cost(self,vertex):
+        distance=0
+        for i in range(len(vertex)):
+            if i+1==len(vertex):
+                distance=distance+int(self.matrix[vertex[i]][vertex[0]])
+                break
+            distance=distance+int(self.matrix[vertex[i]][vertex[i+1]])
+        return distance
+
+    def two_opt(self):
+        path = [x for x in range(self.dimension)]
+        best = path
+        improved = True
+        while improved:
+            improved = False
+            for i in range(0, len(path)-1):
+                for j in range(i+1, len(path)):
+                    if j-i == 1: continue
+                    new_route = path[:]
+                    new_route[i:j] = reversed(new_route[i:j])
+                    if self.cost(new_route) < self.cost(best):
+                        best = new_route
+                        improved = True
+            path = best
+        print("Droga: ",self.cost(best))
+        print("Cykl: ", path)
+        if self.edge_weight_format == 'EUC_2D':
+            self.draw_solution(best)
+        self.PRD(self.cost(best))
+
     def show_solution(self):
-        print(self.path)
+        print("Metoda k-random: ")
+        self.k_random_method()
+        print("Metoda najbliższego sąsiada: ")
+        self.nearest_neighbor()
+        print("Roszerzona metoda najbliższego sąsiada: ")
+        self.extended_nearest_neighbor()
+        print("Algorytm 2-OPT: ")
+        self.two_opt()
+
+    def draw_solution(self,path):
+        for i in range(0, len(path)-1):
+            ni=self.coordinates[path[i]+1]
+            nj=self.coordinates[path[i+1]+1]
+            plt.plot([ni['x'], nj['x']], [ni['y'], nj['y']], color="red", linewidth=0.1)
+        for node in self.coordinates.values():
+            plt.plot(node['x'], node['y'], color="blue", marker="o", markersize=2)
+        plt.show()
+
+    def PRD(self,x):
+        ref=self.optimal[self.filename]
+        result=100*(x-ref)/ref
+        print("PRD(x):{}%".format(result))
 
     @staticmethod
     def read_numbers(file):
