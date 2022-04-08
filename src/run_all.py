f35544a71f1d065adba7db48d03553dad9df95b3
Witold <47141012+JaWitold@users.noreply.github.com>
2022-04-08 18:03:18 +0200

    idziemy do drugiej części (#5)
    * [Radek] Zaimplementowane algorytmy
    
    * testy
    
    * sprawozdanie latex
    
    * Added my part of summary
    
    Co-authored-by: 41761707 <wojtczakov@gmail.com>

--- src/run_all.py
+++ src/run_all.py
@@ -3,10 +3,9 @@ import time
 
 start = time.time()
 main = os.getcwd()
-tsp = "../vendors/ALL_tsp"
-atsp = "../vendors/ALL_atsp"
+tsp = "..\\vendors\\ALL_tsp"
+atsp = "..\\vendors\\ALL_atsp"
 
-fails = ''
 current = os.getcwd()
 
 
@@ -15,25 +14,25 @@ def run(path, ending):
     os.chdir(path)
     cwd = os.getcwd()
     
-    list = [x for x in os.listdir(cwd) if x.endswith(ending)]
+    dictionaries = [x for x in os.listdir(cwd) if x.endswith(ending)]
+    dictionaries.sort()
     index = 0
     
-    for directory in list:
+    for directory in dictionaries:
         index += 1
         os.chdir(directory)
         cwd = os.getcwd()
-        command = "python.exe " + main + "\\main.py " + cwd + '\\' + os.listdir(cwd)[0]
-        print(f"finished {round(index / len(list) * 100)}%, ({index}/{len(list)})")
-        result = os.system(command)
-        # if result != 0:
-        print(os.listdir(cwd)[0])
+        command = f"C:\\Users\\user\\AppData\\Local\\Programs\\Python\\Python39\\python.exe {current}\\ext_graph.py {current}\\{path}\\{os.listdir(cwd)[0]}\\{os.listdir(cwd)[0]} "
+        print(command)
+        print(f"finished {round(index / len(dictionaries) * 100)}%, ({index}/{len(dictionaries)})")
+        os.system(command)
         os.chdir('..')
     
-    print(list)
+    print(dictionaries)
 
 
-run(tsp, ".tsp")
-os.chdir(current)
+# run(tsp, ".tsp")
+# os.chdir(current)
 run(atsp, ".atsp")
 stop = time.time()
 print(stop - start)
