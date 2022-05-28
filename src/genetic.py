9af61f876da5c9442d874b9a68a8729f7f4d9de5
JaWitold <witekkaras00@gmail.com>
2022-05-28 10:41:10 +0200

    działa PMX, zrobione na numpy

--- src/genetic.py
+++ src/genetic.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from graph import Graph
 import copy
 import math
@@ -5,157 +7,60 @@ import random
 
 
 class Member:
-    def __init__(self, dna: list[int]):
-        self.dna = dna
-        self.cost = 0
-        self.normalized_cost = 0
+    max_cost: int = 0
+    population: Graph
     
-    def set_cost(self, cost: int):
-        self.cost = cost
-
-    def normalize(self, max_cost):
-        self.normalized_cost = self.cost / max_cost
+    def __init__(self, dna):
+        self.dna = dna
+        self.cost = Member.population.cost(dna)
+        self.normalized_cost = 0
+        Member.max_cost = max(Member.max_cost, self.cost)
+    
+    def normalize(self, sum):
+        self.normalized_cost = self.cost / sum
 
 
 class Population(Graph):
     MAX_ITERATIONS: int = 100000
     SELECTION_RATE: float = 0.6
     MUTATION_RATE: float = 0.01
+    POPULATION_MAX_SIZE: int = 128
     
-    population: list[Member] = []
+    population: list[Member]
     iterations: int = 0
     
     def __init__(self):
         super().__init__()
-        self.populationMaxSize = 128
+        self.best = None
+        Member.population = self
     
     def populate(self, param):
         params = [float(x) for x in param.split(",")]
         self.MAX_ITERATIONS = int(params[0])
         self.MUTATION_RATE = params[1]
         self.SELECTION_RATE = params[2]
-        self.populationMaxSize = int(params[3])
-        # generate first generation
+        self.POPULATION_MAX_SIZE = int(params[3])
+        
         self.population = self.generate_population()
-        # do some things
+        self.best = self.find_best(self.population)
+        
         while not self.stopping_condition():
-            # selection, crossover, mutation
-            current_population = self.selection(self.population)
-            young_population = self.crossover(current_population)
-            self.population = self.mutation(young_population)
-            self.iterations += 1
-        
-        # get best member
-        best_path = self.find_best()
-        best_cost = self.cost(best_path.dna)
-        
-        # show results
-        self.path = best_path.dna
-        self.show_solution(best_cost)
-        self.prd(best_cost)
-    
-    def stopping_condition(self):
-        return self.iterations > self.MAX_ITERATIONS
-    
-    def selection(self, population: list[Member]) -> list[Member]:
-        # normalize costs
-        for a in population:
-            a.set_cost(self.cost(a.dna))
-        
-        m = self.find_worst()
-        max_cost = m.cost
-        for p in population:
-            p.normalize(max_cost)
-        
-        # selection
-        new_population = []
-        for i in range(0, math.floor(self.populationMaxSize * self.SELECTION_RATE)):
-            # roulette
-            c = random.random()
-            index = 0
+            self.iter += 1
+            # Member.max_cost = max(self.population, key=lambda x: x.cost).cost
+            # self.population = self.selection(self.population)
+            self.population = self.crossover(self.population)
+            self.population = self.mutation(self.population)
             
-            while c > 0:
-                c -= population[index].normalized_cost
-                index += 1
-            new_population.append(population[index - 1])
-        return new_population
-    
-    def crossover(self, population: list[Member]) -> list[Member]:
-        """
-        https://user.ceng.metu.edu.tr/~ucoluk/research/publications/tspnew.pdf
-        """
-        new_population = []
-        while len(new_population) < self.populationMaxSize:
-            # get 2 parents randomly
-            [p1, p2] = random.choices(population, k=2)
-            dna1, dna2 = p1.dna, p2.dna
-            # PMX
-            crossover_point = random.randint(0, len(dna1))
-            new_dna = copy.deepcopy(dna1)
-            for i in range(0, crossover_point):
-                p = dna1[i]
-                q = dna2[i]
-                new_dna[i] = q
-                new_dna[new_dna.index(q)] = p
-            new_population.append(Member(new_dna))
-        return new_population
-    
-    def mutation(self, population: list[Member]) -> list[Member]:
-        # SWAP
-        # for s in population:
-        #     if self.MUTATION_RATE < random.random():
-        #         e1, e2 = random.choices(s.dna, k=2)
-        #         i1, i2 = s.dna.index(e1), s.dna.index(e2)
-        #         s.dna[i1], s.dna[i2] = s.dna[i2], s.dna[i1]
-        for s in population:
-            # uncomment for swap
-            # new_candidate = copy.deepcopy(candidate)
-            mock_candidate = copy.deepcopy(s.dna)
-            node_1 = random.randint(1, len(s.dna) - 1)
-            # node_2 = node_1 - 1
-            node_2 = random.randint(node_1, len(s.dna) - 1)
-            # swap
-            if node_1 == node_2:
-                continue
-            # print("NODES: {}, {}".format(node_1,node_2))
-            # invert
-            to_reverse = mock_candidate[node_1:node_2]
-            new_candidate = mock_candidate[:node_1] + to_reverse[::-1] + mock_candidate[node_2:]
-            population.remove(s)
-            population.append(Member(new_candidate))
-        return population
+            best = self.find_best(self.population)
+            if best.cost < self.best.cost:
+                self.best = best
+                self.prd(self.best.cost)
 
-    def find_best(self) -> Member:
-        best = self.population[0]
-        best_cost = self.cost(best.dna)
-        for p in self.population:
-            p_cost = self.cost(p.dna)
-            if best_cost > p_cost:
-                best_cost = p_cost
-                best = p
-        return best
-    
-    def find_worst(self) -> Member:
-        worst = self.population[0]
-        worst_cost = self.cost(worst.dna)
-        for p in self.population:
-            p_cost = self.cost(p.dna)
-            if worst_cost < p_cost:
-                worst_cost = p_cost
-                worst = p
-        return worst
-    
-    def generate_population(self) -> list[Member]:
-        """
-        generate starting population
-        :return: population
-        """
-        population = []
-        for i in range(1, self.populationMaxSize):
-            new_member = [x for x in range(self.dimension)]
-            random.shuffle(new_member)
-            population.append(Member(new_member))
-        return population
+        # self.show_matrix()
+        
+        self.path = self.best.dna
+        self.show_solution(self.best.cost)
+        self.prd(self.best.cost)
     
     def run(self, algorithm, param):
         if algorithm == 'genetic':
@@ -172,3 +77,94 @@ class Population(Graph):
             self.tabu(param)
         else:
             print("Unsupported algorithm")
+    
+    def generate_population(self) -> list[Member]:
+        population = []
+        dna = np.array(range(0, self.dimension))
+        for i in range(0, self.POPULATION_MAX_SIZE):
+            new_dna = dna.copy()
+            np.random.shuffle(new_dna)
+            population.append(Member(new_dna))
+        return population
+    
+    @staticmethod
+    def find_best(population: list[Member]) -> Member:
+        return min(population, key=lambda x: x.cost)
+    
+    def selection(self, population: list[Member]) -> list[Member]:
+        """
+        During each successive generation, a portion of the existing population is selected to breed a new
+        generation. Individual solutions are selected through a fitness-based process, where fitter solutions (as
+        measured by a fitness function) are typically more likely to be selected. Certain selection methods rate the
+        fitness of each solution and preferentially select the best solutions. Other methods rate only a random
+        sample of the population, as the former process may be very time-consuming.
+        """
+        self.normalize(population)
+        new_population = []
+        for _ in range(0, self.POPULATION_MAX_SIZE):
+            index = 0
+            c = random.random()
+            while c > 0:
+                c -= population[index].normalized_cost
+                index += 1
+            index -= 1
+            new_population.append(copy.deepcopy(population[index]))
+        return new_population
+
+    @staticmethod
+    def normalize(population: list[Member]):
+        s = sum(x.cost for x in population)
+        for p in population:
+            p.normalize(s)
+    
+    def crossover(self, population: list[Member]) -> list[Member]:
+        population = sorted(population, key=lambda x: x.cost)
+        new_population = []
+        for t1, t2 in zip(population[::2], population[1::2]):
+            c1, c2 = self.pmx(t1, t2)
+            new_population.append(Member(c1))
+            new_population.append(Member(c2))
+        return new_population
+    
+    def mutation(self, population: list[Member]) -> list[Member]:
+        """
+        Mutation is a genetic operator used to maintain genetic diversity from one generation of a population of
+        genetic algorithm chromosomes to the next. It is analogous to biological mutation.
+        """
+        new_population = []
+        for member in population:
+            if random.random() < self.MUTATION_RATE:
+                i1, i2 = np.random.choice(member.dna.shape[0], 2, replace=False)
+                i1 = 0
+                if i1 > i2:
+                    i1, i2 = i2, i1
+                # new_dna = np.copy(member.dna)
+
+                new_population.append(
+                    Member(
+                        np.concatenate((member.dna[0:i1 + 1], member.dna[i2:i1:-1], member.dna[i2 + 1:]))
+                    )
+                )
+            else:
+                new_population.append(member)
+        return new_population
+    
+    def pmx(self, t1, t2):
+        """
+        Partially mapped crossover (PMX) is one of the most popular and effective crossovers for order-based GAs
+        to deal with combinatorial optimization problems, especially the TSP. In view of the operation, PMX can be
+        regarded as a modification of two-point crossover but additionally uses a mapping relationship to legalize
+        offspring that have duplicate numbers.
+        https://www.redalyc.org/pdf/2652/265224466006.pdf
+        """
+        # t2 = Member(np.array([1, 2, 3, 4]))
+        # t1 = Member(np.array([4, 1, 2, 3]))
+        # self.dimension = 4
+        crossover_point = random.randint(0, self.dimension)
+        child1 = np.copy(t1.dna)
+        child2 = np.copy(t1.dna)
+        for i in range(0, crossover_point):
+            child1[np.where(t2.dna[i] == child1)], child2[np.where(t1.dna[i] == child2)] = child1[i], child2[i]
+            child1[i], child2[i] = t2.dna[i], t1.dna[i]
+        
+        return child1, child2
